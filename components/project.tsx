4790a302d0f34500e0927fb20122561e3daca91d
Parth Patel <parth.patel1@ucalgary.ca>
2024-02-13 11:42:14 -0700

    projects transform fix

--- components/project.tsx
+++ components/project.tsx
@@ -24,8 +24,8 @@ export default function Project({
         target: ref,
         offset: ["0 1", "1.30 1"],
     });
-    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
     return (
         <motion.div
             style={{
