dbe5cc7ca1276784f4a8dc57629b75071b3bd7fb
Parth Patel <parth.patel1@ucalgary.ca>
2025-01-13 23:04:50 -0700

    Update experience

--- lib/data.ts
+++ lib/data.ts
@@ -87,10 +87,10 @@ export const experiencesData = [
     },
     {
         name: "Wealthsimple",
-        title: "Incoming Software Engineer Intern",
+        title: "Software Engineer Intern",
         location: "Calgary, AB",
         description:
-            "Incoming Software Engineer Intern on the Book of Records team",
+            "Software Engineer Intern on the Book of Records team",
         icon: React.createElement(MdWorkOutline),
         date: "Jan 2025",
         logo: wsLogo
