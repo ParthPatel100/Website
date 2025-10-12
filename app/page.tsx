a517ea24fe5877d63cbd99e6fed15567e9e6253a
Parth Patel <parth.patel1@ucalgary.ca>
2025-10-12 14:08:41 -0600

    Remove resend

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 
-import Contact from "@/components/contact";
-
-
 
 export default function Home() {
     return (
@@ -30,7 +27,6 @@ export default function Home() {
             <Projects/>
             <Skills/>
             <Experience/>
-            <Contact/>
 
         </main>
     );
