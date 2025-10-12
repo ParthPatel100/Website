8ea8a699801e91aacfcefe1cdac75c5f7630a3d6
Parth Patel <parth.patel1@ucalgary.ca>
2025-10-12 14:09:35 -0600

    Remove contact

--- lib/data.ts
+++ lib/data.ts
@@ -58,10 +58,6 @@ export const links = [
         name: "Experience",
         hash: "#experience",
     },
-    {
-        name: "Contact",
-        hash: "#contact",
-    },
 ] as const;
 
 export const experiencesData = [
